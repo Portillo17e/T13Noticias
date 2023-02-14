e91ee09dbb740ff74a481caaa5b48815a624609f
Portillo17e <eduardo2portillo789@gmail.com>
2023-02-14 09:35:08 -0600

    final
    final

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Eduardo Dabilo rodriguez Portillo""1646323")
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt','w')
+    archivo.write('Hola mundo')
+    archivo.close()
